ec650d613ca4c189bbf3ce9800bf6a430470052f
yuwonhyeok <dbdnjsgur1@gmail.com>
2021-08-24 12:43:43 +0900

    초기버전

---
+++ src/main/java/com/study/springboot/GittestTempApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestTempApplication.class, args);
+	}
+
+}
